eacf62eb04a307ba28c63083184d305f5b669a5a
Your Name <martinreimer@mac.com>
2025-03-31 09:33:04 -0700

    Testing

--- src/ArrayBuilder.java
+++ src/ArrayBuilder.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class ArrayBuilder {
     public static void main(String[] args) {
-
+        int[] nums = new int[5];
+        System.out.println(Arrays.toString(nums));
     }
 }
